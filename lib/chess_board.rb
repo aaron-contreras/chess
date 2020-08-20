bce6689870ada6bb9fa76877d996a4001eebaa7f
Aaron Contreras <aaronlcaq@gmail.com>
2020-08-20 12:09:21 -0500

    Add tests for initial board configuration

--- lib/chess_board.rb
+++ lib/chess_board.rb
@@ -1,6 +1,54 @@
 # frozen_string_literal: true
 class ChessBoard
+
   def initialize
-    @board = Array.new(8) { Array.new(8) }
+    @board = initial_board_configuration
   end
+
+  def initial_board_configuration
+    [
+        [Rook.new(:white), Knight.new(:white), Bishop.new(:white), Queen.new(:white), King.new(:white), Bishop.new(:white), Knight.new(:white), Rook.new(:white)],
+        [Pawn.new(:white), Pawn.new(:white), Pawn.new(:white), Pawn.new(:white), Pawn.new(:white), Pawn.new(:white), Pawn.new(:white), Pawn.new(:white)],
+        [' '] * 8,
+        [' '] * 8,
+        [' '] * 8,
+        [' '] * 8,
+        [Pawn.new(:black), Pawn.new(:black), Pawn.new(:black), Pawn.new(:black), Pawn.new(:black), Pawn.new(:black), Pawn.new(:black), Pawn.new(:black)],
+        [Rook.new(:black), Knight.new(:black), Bishop.new(:black), Queen.new(:black), King.new(:black), Bishop.new(:black), Knight.new(:black), Rook.new(:black)]
+    ]
+  end
+end
+
+class Pieces
+  def initialize(color)
+    @color = color
+  end
+
+  def ==(other_piece)
+    self.class == other_piece.class
+  end
+end
+
+class Rook < Pieces
+
+end
+
+class Knight < Pieces
+
+end
+
+class Bishop < Pieces
+
+end
+
+class Queen < Pieces
+
+end
+
+class King < Pieces
+
+end
+
+class Pawn < Pieces
+
 end
--- spec/chess_board_spec.rb
+++ spec/chess_board_spec.rb
@@ -21,4 +21,23 @@ describe ChessBoard do
 
     expect(board.size).to eq 64
   end
+
+  context 'when a game starts' do
+    it 'has the proper piece configuration' do
+      board = chess_board.instance_variable_get(:@board)
+
+      initial_board_configuration = [
+        [Rook.new(:white), Knight.new(:white), Bishop.new(:white), Queen.new(:white), King.new(:white), Bishop.new(:white), Knight.new(:white), Rook.new(:white)],
+        [Pawn.new(:white), Pawn.new(:white), Pawn.new(:white), Pawn.new(:white), Pawn.new(:white), Pawn.new(:white), Pawn.new(:white), Pawn.new(:white)],
+        [' '] * 8,
+        [' '] * 8,
+        [' '] * 8,
+        [' '] * 8,
+        [Pawn.new(:black), Pawn.new(:black), Pawn.new(:black), Pawn.new(:black), Pawn.new(:black), Pawn.new(:black), Pawn.new(:black), Pawn.new(:black)],
+        [Rook.new(:black), Knight.new(:black), Bishop.new(:black), Queen.new(:black), King.new(:black), Bishop.new(:black), Knight.new(:black), Rook.new(:black)]
+      ]
+
+      expect(board).to eq initial_board_configuration
+    end
+  end
 end
