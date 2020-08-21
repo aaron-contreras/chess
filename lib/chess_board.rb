f6f1c9fad9a19aab7f7cb854aa48c8445326b196
Aaron Contreras <aaronlcaq@gmail.com>
2020-08-20 22:30:34 -0500

    Set initial board configuration to @board

--- lib/chess_board.rb
+++ lib/chess_board.rb
@@ -1,54 +1,32 @@
 # frozen_string_literal: true
+
+require_relative './rook'
+require_relative './knight'
+require_relative './bishop'
+require_relative './queen'
+require_relative './king'
+require_relative './pawn'
+
+# Represents a chess board
 class ChessBoard
+  # rubocop: disable Metrics/LineLength
+  INITIAL_BOARD_CONFIGURATION = [
+    [Rook.new(:white), Knight.new(:white), Bishop.new(:white), Queen.new(:white), King.new(:white), Bishop.new(:white), Knight.new(:white), Rook.new(:white)],
+
+    8.times.map { Pawn.new(:white) },
+
+    [' '] * 8,
+    [' '] * 8,
+    [' '] * 8,
+    [' '] * 8,
+
+    8.times.map { Pawn.new(:black) },
+
+    [Rook.new(:black), Knight.new(:black), Bishop.new(:black), Queen.new(:black), King.new(:black), Bishop.new(:black), Knight.new(:black), Rook.new(:black)]
+  ].freeze
+  # rubocop: enable Metrics/LineLength
 
   def initialize
-    @board = initial_board_configuration
+    @board = INITIAL_BOARD_CONFIGURATION
   end
-
-  def initial_board_configuration
-    [
-        [Rook.new(:white), Knight.new(:white), Bishop.new(:white), Queen.new(:white), King.new(:white), Bishop.new(:white), Knight.new(:white), Rook.new(:white)],
-        [Pawn.new(:white), Pawn.new(:white), Pawn.new(:white), Pawn.new(:white), Pawn.new(:white), Pawn.new(:white), Pawn.new(:white), Pawn.new(:white)],
-        [' '] * 8,
-        [' '] * 8,
-        [' '] * 8,
-        [' '] * 8,
-        [Pawn.new(:black), Pawn.new(:black), Pawn.new(:black), Pawn.new(:black), Pawn.new(:black), Pawn.new(:black), Pawn.new(:black), Pawn.new(:black)],
-        [Rook.new(:black), Knight.new(:black), Bishop.new(:black), Queen.new(:black), King.new(:black), Bishop.new(:black), Knight.new(:black), Rook.new(:black)]
-    ]
-  end
-end
-
-class Pieces
-  def initialize(color)
-    @color = color
-  end
-
-  def ==(other_piece)
-    self.class == other_piece.class
-  end
-end
-
-class Rook < Pieces
-
-end
-
-class Knight < Pieces
-
-end
-
-class Bishop < Pieces
-
-end
-
-class Queen < Pieces
-
-end
-
-class King < Pieces
-
-end
-
-class Pawn < Pieces
-
 end
