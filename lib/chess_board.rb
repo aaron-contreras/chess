6b84f7fb44765a6aca00395e1d27e890c79a5ab8
Aaron Contreras <aaronlcaq@gmail.com>
2020-08-27 18:30:06 -0500

    Add castling feature

--- lib/chess_board.rb
+++ lib/chess_board.rb
@@ -16,12 +16,43 @@ class ChessBoard
   def move_piece(piece_square, target_square)
     piece_to_move = @board[piece_square[0]][piece_square[1]]
 
-    @board[piece_square[0]][piece_square[1]] = ' '
+    board[piece_square[0]][piece_square[1]] = ' '
 
-    @board[target_square[0]][target_square[1]] = piece_to_move
+    board[target_square[0]][target_square[1]] = piece_to_move
+  end
+
+  def castle(king_coordinates, rook_coordinates)
+    king_target = king_castle_target(king_coordinates, rook_coordinates)
+
+    rook_target = rook_castle_target(king_coordinates, rook_coordinates)
+
+    move_piece(king_coordinates, king_target)
+    move_piece(rook_coordinates, rook_target)
   end
 
   private
+
+  attr_reader :board
+
+  def king_castle_target(king_coordinates, rook_coordinates)
+    if rook_coordinates[1].zero?
+      [king_coordinates[0], king_coordinates[1] - 2]
+    else
+      [king_coordinates[0], king_coordinates[1] + 2]
+    end
+  end
+
+  def rook_castle_target(king_coordinates, rook_coordinates)
+    if rook_coordinates[1].zero?
+      [king_coordinates[0], king_coordinates[1] - 1]
+    else
+      [king_coordinates[0], king_coordinates[1] + 1]
+    end
+  end
+
+  def clear_square(coordinates)
+    board[coordinates[0]][coordinates[1]] = ' '
+  end
 
   def initial_board_configuration
     [
--- spec/chess_board_spec.rb
+++ spec/chess_board_spec.rb
@@ -32,4 +32,64 @@ describe ChessBoard do
       end
     end
   end
+
+  describe '#castle' do
+    let(:board) { chess_board.instance_variable_get(:@board) }
+    let(:king_start_coordinates) { [0, 4] }
+
+    context 'when move is short castle' do
+      let(:king_end_coordinates) { [0, 6] }
+      let(:rook_start_coordinates) { [0, 7] }
+      let(:rook_end_coordinates) { [0, 5] }
+
+      before do
+        board[0][5] = ' '
+        board[0][6] = ' '
+      end
+
+      it 'moves the king two squares towards the rook' do
+        king = board.dig(*king_start_coordinates)
+
+        chess_board.castle(king_start_coordinates, rook_start_coordinates)
+
+        expect(board.dig(*king_end_coordinates)).to eq king
+      end
+
+      it 'moves the rook one square over the king' do
+        rook = board.dig(*rook_start_coordinates)
+
+        chess_board.castle(king_start_coordinates, rook_start_coordinates)
+
+        expect(board.dig(*rook_end_coordinates)).to eq rook
+      end
+    end
+
+    context 'when move is long castle' do
+      let(:king_end_coordinates) { [0, 2] }
+      let(:rook_start_coordinates) { [0, 0] }
+      let(:rook_end_coordinates) { [0, 3] }
+
+      before do
+        board[0][1] = ' '
+        board[0][2] = ' '
+        board[0][3] = ' '
+      end
+
+      it 'moves the king two squares towards the rook' do
+        king = board.dig(*king_start_coordinates)
+
+        chess_board.castle(king_start_coordinates, rook_start_coordinates)
+
+        expect(board.dig(*king_end_coordinates)).to eq king
+      end
+
+      it 'moves the rook one square over the king' do
+        rook = board.dig(*rook_start_coordinates)
+
+        chess_board.castle(king_start_coordinates, rook_start_coordinates)
+
+        expect(board.dig(*rook_end_coordinates)).to eq rook
+      end
+    end
+  end
 end
