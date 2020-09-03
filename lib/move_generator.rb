6f923748523be7b422913fecb045d3eab4933aec
Aaron Contreras <aaronlcaq@gmail.com>
2020-09-03 12:22:41 -0500

    Add tests for repeated jump pieces

--- lib/move_generator.rb
+++ lib/move_generator.rb
@@ -1,4 +1,62 @@
 # frozen_string_literal: true
 
+# Generates the valid set of moves available in a ChessBoard
 class MoveGenerator
+  def initialize(chess_board)
+    @chess_board = chess_board
+  end
+
+  def generate_moves(piece_coordinates)
+    piece = chess_board.board.dig(*piece_coordinates)
+
+    if piece.repeatedly_jumps?
+      moves_by_direction = piece.movement_directions.map do |y_travel, x_travel|
+        1.upto(8).map do |jump|
+          [piece_coordinates[0] + jump * y_travel, piece_coordinates[1] + jump * x_travel]
+        end
+      end
+    else
+      moves_by_direction = piece.movement_directions.map do |y_travel, x_travel|
+        [[piece_coordinates[0] + y_travel, piece_coordinates[1] + x_travel]]
+      end
+    end
+
+    valid_moves_by_direction = moves_by_direction.map do |movements|
+      movements.select do |move|
+        valid_square?(move)
+      end
+    end.reject(&:empty?)
+
+    final_moves = valid_moves_by_direction.map do |movements_by_direction|
+      first_invalid = movements_by_direction.find_index do |move|
+        square_contents = chess_board.board.dig(*move)
+
+        (square_contents != ' ' && square_contents.color == piece.color)
+      end
+
+      # Here
+
+      movements_by_direction[0...first_invalid]
+    end.reject(&:empty?)
+
+    if piece.repeatedly_jumps?
+      final_moves
+    else
+      final_moves.map do |direction|
+        direction[0]
+      end
+    end
+  end
+
+  private
+
+  attr_reader :chess_board
+
+  def valid_square?(move)
+    move.all? { |coord| coord.between?(0, 8) }
+  end
+
+  def valid_move?(move)
+  end
+
 end
--- spec/move_generator_spec.rb
+++ spec/move_generator_spec.rb
@@ -7,29 +7,6 @@ require_relative '../lib/move_generator'
 describe MoveGenerator do
   # Has a ChessBoard object as a dependency
 
-  let(:board_layout) do
-    [
-      8.times.map { double('White Pawn') },
-      [
-        double('White Rook Left'), double('White Knight Left'), double('White Bishop Left'),
-        double('White Queen'), double('White King'),
-        double('White Bishop Right'), double('White Knight Right'), double('White Rook Right')
-      ],
-
-      8.times.map { ' ' },
-      8.times.map { ' ' },
-      8.times.map { ' ' },
-      8.times.map { ' ' },
-
-      8.times.map { double('Black Pawn') },
-      [
-        double('Black Rook Left'), double('Black Knight Left'), double('Black Bishop Left'),
-        double('Black Queen'), double('Black King'),
-        double('Black Bishop Right'), double('Black Knight Right'), double('Black Rook Right')
-      ]
-    ]
-  end
-
   let(:chess_board) { double('ChessBoard', board: board_layout) }
 
   subject(:generator) { described_class.new(chess_board) }
@@ -37,15 +14,126 @@ describe MoveGenerator do
   # Test pieces without special cases for capturing first
   describe 'generate_move' do
     context 'on initial board configuration' do
+      let(:board_layout) do
+        [
+          [
+            double('White Rook Left'), double('White Knight Left'), double('White Bishop Left'),
+            double('White Queen'), double('White King'),
+            double('White Bishop Right'), double('White Knight Right'), double('White Rook Right')
+          ],
+          8.times.map { double('White Pawn') },
+
+          8.times.map { ' ' },
+          8.times.map { ' ' },
+          8.times.map { ' ' },
+          8.times.map { ' ' },
+
+          8.times.map { double('Black Pawn') },
+          [
+            double('Black Rook Left'), double('Black Knight Left'), double('Black Bishop Left'),
+            double('Black Queen'), double('Black King'),
+            double('Black Bishop Right'), double('Black Knight Right'), double('Black Rook Right')
+          ]
+        ]
+      end
+
+      before do
+        board_layout[0].each do |white_piece|
+          allow(white_piece).to receive(:color).and_return(:white)
+        end
+
+        board_layout[1].each do |white_piece|
+          allow(white_piece).to receive(:color).and_return(:white)
+        end
+      end
+
       context 'when given a rook' do
-        # TO-DO: fails
-        it 'generates its valid moves' do
-          pawn_coordinates = [1, 3]
-          valid_moves = [[2, 3], [3, 3]]
+        let(:rook_movement_directions) { [[-1, 0], [0, 1], [1, 0], [0, -1]] }
 
-          generated_moves = generator.generate_move(pawn_coordinates)
+        it 'returns no valid moves' do
+          allow(board_layout[0][0]).to receive(:movement_directions).and_return(rook_movement_directions)
+          allow(board_layout[0][0]).to receive(:repeatedly_jumps?).and_return(true)
+          valid_moves = generator.generate_moves([0, 0])
+          expect(valid_moves).to eq([])
+        end
+      end
 
-          expect(generated_moves).to eq valid_moves
+      context 'when given a bishop' do
+        let(:bishop_movement_directions) { [[-1, -1], [-1, 1], [1, 1], [1, -1]] }
+
+        it 'returns no valid moves' do
+          allow(board_layout[0][2]).to receive(:movement_directions).and_return(bishop_movement_directions)
+          allow(board_layout[0][2]).to receive(:repeatedly_jumps?).and_return(true)
+
+          valid_moves = generator.generate_moves([0, 2])
+
+          expect(valid_moves).to eq([])
+        end
+      end
+
+      # THESE DON'T REPEATEDLY JUMP
+      # CODE THESE LATER
+      context 'when given a knight' do
+        let(:knight_movement_directions) do
+          [
+            [-1, -2], [-2, -1],
+            [-2, 1], [-1, 2],
+            [1, 2], [2, 1],
+            [1, -2], [2, -1]
+          ]
+        end
+
+        it 'returns two valid moves' do
+          allow(board_layout[0][1]).to receive(:movement_directions).and_return(knight_movement_directions)
+          allow(board_layout[0][1]).to receive(:repeatedly_jumps?).and_return(false)
+
+          valid_moves = generator.generate_moves([0, 1])
+
+          expect(valid_moves).to contain_exactly([2, 0], [2, 2])
+        end
+      end
+      # KING
+
+      context 'when given a king' do
+        let(:king_movement_directions) do
+          [
+            [-1, -1],
+            [-1, 0],
+            [-1, -1],
+            [0, 1],
+            [1, 1],
+            [1, 0],
+            [1, -1],
+            [0, -1]
+          ]
+        end
+
+        it 'returns no valid moves' do
+          allow(board_layout[0][4]).to receive(:movement_directions).and_return(king_movement_directions)
+          allow(board_layout[0][4]).to receive(:repeatedly_jumps?).and_return(false)
+
+          valid_moves = generator.generate_moves([0, 4])
+
+          expect(valid_moves).to eq([])
+        end
+      end
+
+      # SPECIAL CASES FOR CAPTURES
+
+      # PAWN
+
+      # SPECIAL CASES
+
+      context 'when given a queen' do
+        let(:queen_movement_directions) { [[-1, 0], [0, 1], [1, 0], [0, -1], [-1, -1], [-1, 1], [1, 1], [1, -1]] }
+
+        it 'returns no valid moves' do
+          allow(board_layout[0][3]).to receive(:movement_directions).and_return(queen_movement_directions)
+          allow(board_layout[0][3]).to receive(:repeatedly_jumps?).and_return(true)
+
+          valid_moves = generator.generate_moves([0, 3])
+
+          expect(valid_moves).to eq([])
         end
       end
     end
