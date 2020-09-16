a55cb38bcd4e26b99e2e3efc1c636ef9e7c05028
Aaron Contreras <aaronlcaq@gmail.com>
2020-09-15 19:52:00 -0500

    Abstract chess pieces into a module

--- lib/pawn.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# A Pawn chess piece
-class Pawn
-  attr_reader :color
-  def initialize(color)
-    @color = color
-  end
-end
--- lib/rook.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# A Rook chess piece
-class Rook
-  attr_reader :color
-  def initialize(color)
-    @color = color
-  end
-end
