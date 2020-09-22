de3faa8b7c5f6c4b31a65ae318e909a82d032b7f
Aaron Contreras <aaronlcaq@gmail.com>
2020-09-22 06:09:33 -0500

    Add tests for bishop movement

--- spec/chess_pieces/bishop_spec.rb
+++ spec/chess_pieces/bishop_spec.rb
@@ -55,6 +55,30 @@ RSpec.describe Bishop do
         expect(list_of_moves).to be_empty
       end
     end
+
+    context 'when in a position to move in two directions' do
+      subject(:bishop) { described_class.new(:black, [5, 3]) }
+
+      it 'returns an array with all valid moves/captures' do
+        other_pieces = pieces.reject.with_index { |_piece, index| index == 27 }        
+
+        list_of_moves = bishop.moves(other_pieces)
+
+        expect(list_of_moves).to contain_exactly([4, 2], [3, 1], [2, 0], [4, 4], [3, 5], [2, 6], [1, 7])
+      end
+    end
+
+    context 'when in a position to move in all directions' do
+      subject(:bishop) { described_class.new(:white, [3, 5]) }
+
+      it 'returns an array with all valid moves/captures' do
+        other_pieces = pieces.reject.with_index { |_piece, index| index == 5 }
+
+        list_of_moves = bishop.moves(other_pieces)
+
+        expect(list_of_moves).to contain_exactly([2, 4], [2, 6], [4, 6], [5, 7], [4, 4], [5, 3], [6, 2])
+      end
+    end
   end
 end
 # rubocop: enable all
