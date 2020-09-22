0ece954c6df222981830579f070502377f5dec49
Aaron Contreras <aaronlcaq@gmail.com>
2020-09-22 15:33:11 -0500

    Refactor StandardMoveGenerator with base class

--- lib/generators/standard_move_generator.rb
+++ lib/generators/standard_move_generator.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
+require_relative 'generator'
+
 # Generates all moves for standard moving pieces (Rooks and Bishops) based on
 # the directions in which they move.
 # They are stopped from further jumping by friendly pieces and a first capture
 # encounter.
-class StandardMoveGenerator
+class StandardMoveGenerator < Generator
   def initialize(piece, other_pieces, jump_directions)
     @piece = piece
     @other_pieces = other_pieces
@@ -18,8 +20,6 @@ class StandardMoveGenerator
   end
 
   private
-
-  attr_reader :piece, :other_pieces, :jump_directions
 
   def movable_locations(direction, has_not_captured = true)
     locations_in_direction = []
@@ -35,32 +35,6 @@ class StandardMoveGenerator
     end
 
     locations_in_direction
-  end
-
-  def next_jump(position, direction)
-    [position[0] + direction[0], position[1] + direction[1]]
-  end
-
-  def valid_position?(position)
-    inbound?(position) && (no_piece_at?(position) || other_player_is_at?(position))
-  end
-
-  def inbound?(position)
-    position[0].between?(0, 7) && position[1].between?(0, 7)
-  end
-
-  def no_piece_at?(position)
-    other_pieces.none? { |other_piece| other_piece.position == position }
-  end
-
-  def piece_at(position)
-    other_pieces.select { |other_piece| other_piece.position == position }[0]
-  end
-
-  def other_player_is_at?(position)
-    other_piece = piece_at(position)
-
-    other_piece.player != piece.player
   end
 
   def capture?(position)
