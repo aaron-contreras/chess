db10c061c2670dd3f36af8bc2b5b0c09418a86bb
Aaron Contreras <aaronlcaq@gmail.com>
2020-09-22 06:00:59 -0500

    Improve Rook's documentation

--- lib/chess_pieces/rook.rb
+++ lib/chess_pieces/rook.rb
@@ -2,7 +2,8 @@
 
 require_relative '../generators/standard_move_generator'
 
-# A regular-moving piece. Jumps repeatedly perperndicularly
+# A regular-moving piece.
+# Jumps repeatedly accross the board in a perpendicular direction.
 class Rook
   attr_reader :player, :position
 
@@ -13,6 +14,7 @@ class Rook
 
   def moves(other_pieces)
     jump_directions = [[-1, 0], [0, 1], [1, 0], [0, -1]]
+
     generator = StandardMoveGenerator.new(self, other_pieces, jump_directions)
 
     generator.generate_moves
