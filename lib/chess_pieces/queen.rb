16636ef1b48c90c269c503473c7dcbc376cbf7fa
Aaron Contreras <aaronlcaq@gmail.com>
2020-09-22 06:48:23 -0500

    Add tests + implementation for Queen class

---
+++ lib/chess_pieces/queen.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require_relative '../generators/standard_move_generator'
+
+class Queen
+  attr_reader :player, :position
+
+  def initialize(player, position)
+    @player = player
+    @position = position
+  end
+
+  def moves(other_pieces)
+    jump_directions = [[-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1]]
+
+    generator = StandardMoveGenerator.new(self, other_pieces, jump_directions)
+
+    generator.generate_moves
+  end
+
+  def to_s
+    if player == :white
+      "\u2655"
+    else
+      "\u265b"
+    end
+  end
+end
