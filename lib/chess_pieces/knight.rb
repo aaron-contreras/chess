10d2cab281a04aeb9a8c041d36cb401b7c949115
Aaron Contreras <aaronlcaq@gmail.com>
2020-09-22 14:45:31 -0500

    Add code for knight moves

---
+++ lib/chess_pieces/knight.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require_relative 'chess_piece'
+
+class Knight < ChessPiece
+  def initialize(player, position)
+    super(player, position)
+  end
+
+  def moves(other_pieces)
+    jump_directions = [[-2, 1], [-1, 2], [1, 2], [2, 1], [2, -1] [1, -2], [-1, -2], [-2, -1]]
+
+    generator = SingleJumpMoveGenerator.new(self, other_pieces, jump_directions)
+
+    generator.generate_moves
+  end
+end
