bbcba75f32b865accb4ee17b9e219c5bdc433438
Aaron Contreras <aaronlcaq@gmail.com>
2020-09-22 15:33:33 -0500

    Fix typo on jump_directions

--- lib/chess_pieces/knight.rb
+++ lib/chess_pieces/knight.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative 'chess_piece'
+require_relative '../generators/single_jump_move_generator'
 
 class Knight < ChessPiece
   def initialize(player, position)
@@ -8,7 +9,7 @@ class Knight < ChessPiece
   end
 
   def moves(other_pieces)
-    jump_directions = [[-2, 1], [-1, 2], [1, 2], [2, 1], [2, -1] [1, -2], [-1, -2], [-2, -1]]
+    jump_directions = [[-2, 1], [-1, 2], [1, 2], [2, 1], [2, -1], [1, -2], [-1, -2], [-2, -1]]
 
     generator = SingleJumpMoveGenerator.new(self, other_pieces, jump_directions)
 
