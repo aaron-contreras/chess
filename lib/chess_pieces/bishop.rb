81da4af02c312ebcc8d0fe4eb717784c8378017a
Aaron Contreras <aaronlcaq@gmail.com>
2020-09-22 06:02:05 -0500

    Add test and code for bishops

--- lib/chess_pieces/bishop.rb
+++ lib/chess_pieces/bishop.rb
@@ -2,4 +2,20 @@
 
 require_relative '../generators/standard_move_generator'
 
-class Bishop; end
+# A regular moving piece.
+# Jumps repeatedly across the board in a diagonal direction.
+class Bishop
+  attr_reader :player, :position
+  def initialize(player, position)
+    @player = player
+    @position = position
+  end
+
+  def moves(other_pieces)
+    jump_directions = [[-1, -1], [-1, 1], [1, 1], [1, -1]]
+
+    generator = StandardMoveGenerator.new(self, other_pieces, jump_directions)
+
+    generator.generate_moves
+  end
+end
--- spec/chess_pieces/bishop_spec.rb
+++ spec/chess_pieces/bishop_spec.rb
@@ -45,10 +45,14 @@ RSpec.describe Bishop do
 
   describe '#moves' do
     context 'when not in a position to move' do
+      subject(:bishop) { described_class.new(:white, [0, 5]) }
+
       it 'returns an empty array' do
         other_pieces = pieces.reject.with_index { |_piece, index| index == 2 }
 
+        list_of_moves = bishop.moves(other_pieces)
 
+        expect(list_of_moves).to be_empty
       end
     end
   end
