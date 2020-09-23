f4fc6bd68771ffadc61d729e9d6e9b005d8fc863
Aaron Contreras <aaronlcaq@gmail.com>
2020-09-23 12:30:58 -0500

    Add helper methods for movement and direction

--- lib/chess_pieces/pawn.rb
+++ lib/chess_pieces/pawn.rb
@@ -1,13 +1,37 @@
 # frozen_string_literal: true
 
 require_relative 'chess_piece'
-
+require_relative '../generators/pawn_move_generator'
 class Pawn < ChessPiece
   def initialize(player, position)
     super(player, position)
   end
 
   def moves(other_pieces)
-    []
+    generator = PawnMoveGenerator.new(self, other_pieces, jump_directions, capture_directions)
+
+    generator.generate_moves
+  end
+
+  private
+
+  def color_direction
+    if player == :white
+      1
+    else
+      -1
+    end
+  end
+
+  def jump_directions
+    if moved
+      [[1 * color_direction, 0]]
+    else
+      [[1 * color_direction, 0], [2 * color_direction, 0]]
+    end
+  end
+
+  def capture_directions
+    [[1 * color_direction, -1], [1 * color_direction, 1]]
   end
 end
