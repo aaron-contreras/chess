4a1dfd0aafee7c2a0f8c1db22b2bd7204beeda36
Aaron Contreras <aaronlcaq@gmail.com>
2020-09-23 12:12:35 -0500

    Add first spec for pawns

--- spec/chess_pieces/pawn_spec.rb
+++ spec/chess_pieces/pawn_spec.rb
@@ -1,7 +1,22 @@
 # frozen_string_literal: true
 
 require_relative '../../lib/chess_pieces/pawn'
+require_relative '../shared/among_chess_pieces.rb'
 
 RSpec.describe Pawn do
-  it 'works' do; end
+  include_context 'list_of_pieces'
+  
+  describe '#moves' do
+    context "when it hasn't yet moved" do
+      subject(:pawn) { described_class.new(:white, [1, 1]) }
+
+      it 'returns a single and double jump move' do
+        other_pieces = pieces.reject.with_index { |_piece, index| index == 9 }
+
+        list_of_moves = pawn.moves(other_pieces)
+
+        expect(list_of_moves).to contain_exactly([2, 1], [3, 1])
+      end
+    end
+  end
 end
