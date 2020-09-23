fe43f9f75638d5625de23a7b75ab6648a5485822
Aaron Contreras <aaronlcaq@gmail.com>
2020-09-23 06:39:19 -0500

    Make #moves 'abstract' in the base class

--- lib/chess_pieces/chess_piece.rb
+++ lib/chess_pieces/chess_piece.rb
@@ -1,10 +1,15 @@
 # frozen_string_literal: true
 
+# Base class with common attributes and behavior for a chess piece
 class ChessPiece
   attr_reader :player, :position
 
   def initialize(player, position)
     @player = player
     @position = position
+  end
+
+  def moves(other_pieces)
+    raise NotImplementedError, 'Implement to allow your chess piece to know how to move'
   end
 end
