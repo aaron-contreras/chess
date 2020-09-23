c86b10d36f6c05791ab65431620c20192725ecfc
Aaron Contreras <aaronlcaq@gmail.com>
2020-09-23 12:14:54 -0500

    Add skeleton for Pawn class

--- lib/chess_pieces/pawn.rb
+++ lib/chess_pieces/pawn.rb
@@ -1,3 +1,13 @@
 # frozen_string_literal: true
 
-class Pawn; end
+require_relative 'chess_piece'
+
+class Pawn < ChessPiece
+  def initialize(player, position)
+    super(player, position)
+  end
+
+  def moves(other_pieces)
+    []
+  end
+end
