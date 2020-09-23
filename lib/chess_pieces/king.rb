229347bbd66570c3071e85a2da3d6d2a380858ee
Aaron Contreras <aaronlcaq@gmail.com>
2020-09-23 06:49:50 -0500

    Add code for King class

---
+++ lib/chess_pieces/king.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require_relative 'chess_piece'
+require_relative '../generators/single_jump_move_generator'
+
+class King < ChessPiece
+  def initialize(player, position)
+    super(player, position)
+  end
+
+  def moves(other_pieces)
+    jump_directions = [[-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1]]
+
+    generator = SingleJumpMoveGenerator.new(self, other_pieces, jump_directions)
+
+    generator.generate_moves
+  end
+end
