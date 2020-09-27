b105f9dcff868ba9f61da7437684f43ff76be2ba
Aaron Contreras <aaronlcaq@gmail.com>
2020-09-27 07:44:10 -0500

    Fix queen specs with new move format

--- spec/chess_pieces/queen_spec.rb
+++ spec/chess_pieces/queen_spec.rb
@@ -34,7 +34,16 @@ RSpec.describe Queen do
 
         list_of_moves = queen.moves(other_pieces)
 
-        expect(list_of_moves).to contain_exactly([6, 2], [5, 1], [4, 0], [6, 4], [5, 5], [4, 6], [3, 7])
+        expected_moves = [
+          { type: :standard, piece: queen, ending_position: [6, 2] },
+          { type: :standard, piece: queen, ending_position: [5, 1] },
+          { type: :standard, piece: queen, ending_position: [4, 0] },
+          { type: :standard, piece: queen, ending_position: [6, 4] },
+          { type: :standard, piece: queen, ending_position: [5, 5] },
+          { type: :standard, piece: queen, ending_position: [4, 6] },
+          { type: :standard, piece: queen, ending_position: [3, 7] }
+        ]
+        expect(list_of_moves).to contain_exactly(*expected_moves)
       end
     end
 
@@ -42,11 +51,37 @@ RSpec.describe Queen do
       subject(:queen) { described_class.new(:white, [3, 3])}
 
       it 'returns all valid moves/captures' do
+        captured_pawn_left = pieces[16]
+        captured_pawn_middle = pieces[19]
+        captured_pawn_right = pieces[22]
+
         other_pieces = pieces.reject.with_index { |_piece, index| index == 3}
 
         list_of_moves = queen.moves(other_pieces)
 
-        expect(list_of_moves).to contain_exactly([2, 3], [2, 4], [3, 4], [3, 5], [3, 6], [3, 7], [4, 4], [5, 5], [6, 6], [4, 3], [5, 3], [6, 3], [4, 2], [5, 1], [6, 0], [3, 2], [3, 1], [3, 0], [2, 2])
+        expected_moves = [
+          { type: :capture, piece: queen, captured_piece: captured_pawn_left, ending_position: [6, 0] },
+          { type: :capture, piece: queen, captured_piece: captured_pawn_middle, ending_position: [6, 3] },
+          { type: :capture, piece: queen, captured_piece: captured_pawn_right, ending_position: [6, 6] },
+          { type: :standard, piece: queen, ending_position: [2, 3] },
+          { type: :standard, piece: queen, ending_position: [2, 4] },
+          { type: :standard, piece: queen, ending_position: [3, 4] },
+          { type: :standard, piece: queen, ending_position: [3, 5] },
+          { type: :standard, piece: queen, ending_position: [3, 6] },
+          { type: :standard, piece: queen, ending_position: [3, 7] },
+          { type: :standard, piece: queen, ending_position: [4, 4] },
+          { type: :standard, piece: queen, ending_position: [5, 5] },
+          { type: :standard, piece: queen, ending_position: [4, 3] },
+          { type: :standard, piece: queen, ending_position: [5, 3] },
+          { type: :standard, piece: queen, ending_position: [4, 2] },
+          { type: :standard, piece: queen, ending_position: [5, 1] },
+          { type: :standard, piece: queen, ending_position: [3, 2] },
+          { type: :standard, piece: queen, ending_position: [3, 1] },
+          { type: :standard, piece: queen, ending_position: [3, 0] },
+          { type: :standard, piece: queen, ending_position: [2, 2] }
+        ]
+
+        expect(list_of_moves).to contain_exactly(*expected_moves)
       end
     end
   end
