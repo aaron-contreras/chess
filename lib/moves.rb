7ff302c695e8215cec8a54c0bb565ece3cb88e44
Aaron Contreras <aaronlcaq@gmail.com>
2020-09-28 11:10:21 -0500

    Delete old implementation of Board

--- lib/moves.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-# Types of moves one can perform in the game.
-module Moves
-  Castling = Struct.new(:style, :king, :rook)
-  EnPassant = Struct.new(:pawn, :enemy_pawn, :ending_position)
-end
